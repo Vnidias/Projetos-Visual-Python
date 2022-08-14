dd41e391aef264e2911c7b501d607cdefecf7699
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 15:54:53 -0300

    commit de funções matematicas

---
+++ mathfunctions.py
@@ -0,0 +1,22 @@
+import math
+from tkinter import Y
+
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+
+
+#print(round(pi)) #faz o arredondamento
+#print(math.ceil(pi)) #arredonda para o proximo numero inteiro
+#print(math.floor(pi)) #arredonda para o numero anterior inteiro
+#print(abs(pi)) #traz o valor absoluto
+#print(pow(pi,3)) # eleva ao numero passado após a virgula
+#print(math.sqrt(pi)) # raiz quadrada de um valor.
+#print(max(x,y,z)) # retorna o valor maximo
+#print(min(x,y,z)) # retorna o valor minimo
+
+
