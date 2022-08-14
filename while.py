fa8e4ba14f32df83c650f4e0e7ff64c9944d6585
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 17:09:36 -0300

    whileexercise

---
+++ while.py
@@ -0,0 +1,10 @@
+#while loop = executa um laço de acordo com a condição.
+
+# while 1==1: # loop infinito
+#     print("Help! Im stuck in loop")
+    
+name = ""
+while len(name) == 0: # loop infinito
+    name = input("enter your name: ")
+    
+print("hello " + name)   
