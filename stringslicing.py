4c90970609a79437d98be763e3cc29e5c689293e
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 16:13:20 -0300

    commit de slice1

--- stringslicing.py
+++ stringslicing.py
@@ -1,11 +1,17 @@
 #slicing = creating a substring from a element
 # comumente construido no seguinte formado indexing[] or slice()
                 # regra [start:stop:step]
+                # caso voce nao preencha o start e o stop ele assumira no start o começo da string e no stop
+                # o final da string
                 
-name = input("Digite seu nome completo utilizando espacos e sem caracteres especiais: ")
+#name = input("Digite seu nome completo utilizando espacos e sem caracteres especiais: ")
 
+name = "Vinicius Dias Barros"
 firstname = name[0:8:1]
+lastname = name[8:13:1]
 print("meu firstname é:" + firstname)
+print("meu sobrenome é:" + lastname)
+
 nome, sobrenome, ultimonome = name.split(" ")
 
 print(nome)
