d24be0005a0e7f9ff7301c3227ecbfea019b4bd2
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 16:55:44 -0300

    vinicius_dias_barros_14082022

---
+++ and_or_not.py
@@ -0,0 +1,9 @@
+temp = int(input("whats the temperature outside?: "))
+
+if temp >= 0 and temp <= 30: 
+    print("the temperature is good today!")
+    print("go outside!")
+    
+elif temp < 0 or temp > 30: 
+    print("the temperature is bad today!")
+    print("stay inside")
