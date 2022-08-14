419af1aa4417fe1d7b09ba0c86c6535ba67c1d27
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 17:22:10 -0300

    conflict solved

--- for.py
+++ for.py
@@ -8,8 +8,13 @@ import time
 # for i in range(10):
 #     print(i+1)
     
-for i in range(50,100+1,2): #start,stop,step
-    print(i)
+# for i in range(50,100+1,2): #start,stop,step
+#     print(i)
     
-# for i in range(10):
-#     print(i+1)
+# for i in "Bro Code":
+#     print(i)
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year")
