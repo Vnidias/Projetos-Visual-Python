22762710b8b5453d17bfd77c157884419ce65af1
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 17:15:20 -0300

    for

---
+++ for.py
@@ -0,0 +1,15 @@
+from importlib import import_module
+
+
+import time
+    #for loop limited
+    #while loop unlimited
+    
+# for i in range(10):
+#     print(i+1)
+    
+for i in range(50,100+1,2): #start,stop,step
+    print(i)
+    
+# for i in range(10):
+#     print(i+1)
