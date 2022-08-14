18f57f5377ebf339f6c57c0436a697509d5d6040
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 17:01:25 -0300

    and_or_not_14082022

--- and_or_not.py
+++ and_or_not.py
@@ -7,3 +7,13 @@ if temp >= 0 and temp <= 30:
 elif temp < 0 or temp > 30: 
     print("the temperature is bad today!")
     print("stay inside")
+    
+# Exemplo invertido usando o not
+    
+# if temp >= 0 and temp <= 30: 
+#     print("the temperature is good today!")
+#     print("go outside!")
+    
+# elif temp < 0 or temp > 30: 
+#     print("the temperature is bad today!")
+#     print("stay inside")
