75e3880814cbee28711809981a88d869932572e2
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 15:46:49 -0300

    new comit

---
+++ inputtraining.py
@@ -0,0 +1,5 @@
+name = input("Whats is yout name?:   ")
+age = input("Whats is yout age?:   ")
+height = input("Whats is yout height?:   ")
+
+print("Voce se chama " + name + " sua idade é " + str(age) + " e seu peso é " + str(height))
