bb66c41e93b10703b9353e2ac2c52b6892abeb02
Vnidias <viniciusdb200864@gmail.com>
2022-08-14 16:28:35 -0300

    1 if e else

---
+++ if_elif_else.py
@@ -0,0 +1,13 @@
+#if statement = a block of code that will execute if that condition its true.
+
+age = int(input("quantos anos voce tem?: "))
+
+if age == 100:
+    print("Voce esta velho!")
+elif age >=- 18:
+    print("Voce e adulta")
+elif age < 0:
+    print("Voce ainda nao nasceu")
+else:
+    print("voce e uma crianca")
+    
