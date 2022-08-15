7c125dc7a1f22ad5e0f040e60a911b206b6c5344
Vnidias <viniciusdb200864@gmail.com>
2022-08-15 20:37:33 -0300

    novos exemplos commitados set e tupla 15082022

---
+++ tupla.py
@@ -0,0 +1,16 @@
+#tupla = uma coleção que é ordenada e imutavel, usada para agrupar dados relacionados
+
+student = ("bro",21,"male")
+
+#contagem de elementos da tupla
+print(student.count("bro"))
+
+#revela posição do elemento
+print(student.index("bro"))
+
+for x in student:
+    print(x)
+
+if"bro" in student:
+    print("voce encontrou miseravi")
+    
